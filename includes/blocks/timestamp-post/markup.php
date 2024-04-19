2c1049b2fba696e4902184882e600dabd3799d1c
Michael Sumner <75835774+michael-sumner@users.noreply.github.com>
2024-04-19 14:23:23 +0100

    feat(markup): obtain sdcom_previous_certificate_id from post meta

--- includes/blocks/timestamp-post/markup.php
+++ includes/blocks/timestamp-post/markup.php
@@ -7,7 +7,7 @@
 
 namespace SDCOM_Timestamps\Blocks\TimestampPost;
 
-$sdcom_previous_certificate_id = ! empty( $args['sdcomPreviousCertificateId'] ) ? $args['sdcomPreviousCertificateId'] : '';
+$sdcom_previous_certificate_id = get_post_meta( get_the_ID(), 'sdcom_previous_certificate_id', true );
 
 // Bail early if there is no embed_code.
 if ( empty( $sdcom_previous_certificate_id ) ) {
