545578f7ce16d64f5aa40dc19b649ff8b310848a
Michael Sumner <75835774+michael-sumner@users.noreply.github.com>
2024-09-04 16:35:44 +0100

    style(timestamps): typo

--- timestamps.php
+++ timestamps.php
@@ -122,7 +122,7 @@ require_once SDCOM_TIMESTAMPS_INC . 'blocks.php';
 Screen::factory();
 
 /**
- * WP CLI Commands
+ * WP CLI Commands.
  */
 if ( defined( 'WP_CLI' ) && WP_CLI ) {
 	WP_CLI::add_command( 'timestamps', __NAMESPACE__ . '\Command' );
